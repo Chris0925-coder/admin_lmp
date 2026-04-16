efde2cee62a2cecc6ae24532afcdaaa43333bf2d
Chris0925-coder <christianguardia@outlook.es>
2026-04-16 00:44:56 -0500

    0

---
+++ public/scripts/addArticle.js
@@ -0,0 +1,142 @@
+const updateBTN = document.getElementsByName("id");
+const formB = document.getElementById("form-b");
+const fileInput = document.getElementById("images-update");
+const files = fileInput.files;
+const ulrAddArticle = "https://visits-christian-guardias-projects.vercel.app/lovingmypets";
+
+// const addForm = document.getElementById("form");
+const addBTN = document.getElementById("add-btn");
+
+// const formData = new FormData(form);
+// console.log(formData);
+// console.log(fileInput);
+// console.log(files);
+const data = fileInput.addEventListener("change", (e) => {
+  const file = e.target.files[0];
+  // console.log(e);
+  // console.log(file);
+
+  return file;
+});
+
+const token = getCookie("token");
+
+function addArticle() {
+  // addForm.forEach((btn) => {
+  // console.log(btn.value);
+  formB.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    // formData.append("filename", file)
+    let formData = new FormData(formB);
+    // console.log(filename);
+    // console.log(updateBTN);
+    // console.log(formData.get("filename-b"));
+
+    let result = await fetch(ulrAddArticle, {
+      method: "POST",
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        "Content-Type": "application/json; charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Methods": "GET,HEAD,POST,OPTIONS",
+      },
+      body: formData,
+    })
+      .then((response) => {
+        console.log(response);
+        if (response.ok) {
+          alert("Added article successfully!");
+          window.location.reload();
+        } else {
+          alert("Failed to send the form submission.");
+        }
+      })
+      .catch((error) => console.error("Error:", error));
+    // });
+  });
+}
+
+addArticle();
+
+function update() {
+  updateBTN.forEach((btn) => {
+    // console.log(btn.value);
+    btn.addEventListener("click", async function (event) {
+      event.preventDefault();
+      // formData.append("filename", file)
+      let formData = new FormData(form);
+      // const filename = formData.get("filename");
+      // console.log(formData);
+      // console.log(filename);
+      // console.log(updateBTN);
+      // console.log(formData.get("filename-b"));
+
+      let result = await fetch(`/upload/${btn.value}`, {
+        method: "PUT",
+        // headers: {
+        //   "Content-Type": "application/json; charset=utf-8",
+        //   "Access-Control-Allow-Origin": "*",
+        //   "Access-Control-Methods": "GET,PUT,POST,OPTIONS",
+        // },
+        body: formData,
+      })
+        .then((response) => {
+          if (response.ok) {
+            alert("Update article successfully!");
+            window.location.reload();
+          } else {
+            alert("Failed to update the form submission.");
+          }
+        })
+        .catch((error) => console.error("Error:", error));
+
+      // console.log(result);
+    });
+  });
+}
+
+// update();
+
+// const deleteBTN = document.getElementsByName("btn-delete");
+
+function deleteArticle() {
+  deleteBTN.forEach((btn) => {
+    // console.log(btn);
+    btn.addEventListener("click", async function (event) {
+      // const formData = new FormData(form);
+      // console.log(formData);
+      // console.log(updateBTN);
+      // console.log(formData.get("filename-b"));
+      event.preventDefault();
+
+      // console.log("Delete " + btn.value);
+      let result = await fetch(`/upload/${btn.value}`, {
+        method: "DELETE",
+        // headers: {
+        //   "Content-Type": "application/json; charset=utf-8",
+        //   "Access-Control-Allow-Origin": "*",
+        //   "Access-Control-Methods": "GET,HEADERS,POST,OPTIONS",
+        // },
+        body: JSON.stringify({
+          id: btn.value,
+          // title: formData.get("title"),
+          // paragraph: formData.get("paragraph"),
+          // images: formData.get("filename-b"),
+        }),
+      })
+        .then((response) => {
+          if (response.ok) {
+            alert("DELETE article successfully!");
+            window.location.reload();
+          } else {
+            alert("Failed to delete the form submission.");
+          }
+        })
+        .catch((error) => console.error("Error:", error));
+
+      // console.log(result);
+    });
+  });
+}
+
+// deleteArticle();
---
+++ public/scripts/main.js
@@ -0,0 +1,50 @@
+const form = document.querySelector('#form');
+let date = document.querySelector('.date');
+
+const ulr = "https://visits-christian-guardias-projects.vercel.app/lovingmypets/login";
+const message = document.getElementById("message");
+const sectionA = document.querySelector('.submit');
+const sectionB = document.querySelector('.articles');
+
+function submit() {
+	form.addEventListener('submit', async (e) => {
+		e.preventDefault();
+
+		message.style.color = "#009900";
+	    message.innerText = "Iniciando sesion...";
+	    const formData = new FormData(form);
+
+		let result = fetch(url, {
+			method: "POST",
+			body: JSON.stringify({
+		        username: formData.get("username"),
+		        password: formData.get("password"),
+		    }),		
+		})
+		.then((response) => response.json())
+      .catch((error) => {
+        console.error("Error:", error);
+        message.style.color = "#990000";
+        message.innerText = error;
+      });
+
+      if (!result.error) {
+	      setCookie("token", result, 30);
+	      window.location.reload();
+	     // window.location.replace("https://www.crcvpanama.org/pages/admin.html")
+	    } else {
+	      message.style.color = "#990000";
+	      message.innerText = result.error;
+	    }
+	})
+}
+// sectionA.setAttribute("class", "hidden");
+// sectionB.removeAttribute("class", "hidden");
+
+if(detectCookie("token")) {
+	sectionA.setAttribute("class", "hidden");
+	sectionB.removeAttribute("class", "hidden");
+} 
+
+
+date.append(d.getFullYear ());
