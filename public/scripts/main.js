b44c22e3ad73a70952abe71f3cc0ef9fa6e73fab
Chris0925-coder <christianguardia@outlook.es>
2026-04-16 01:04:16 -0500

    0

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,6 @@
 const form = document.querySelector('#form');
 let date = document.querySelector('.date');
-
-const ulr = "https://visits-christian-guardias-projects.vercel.app/lovingmypets/login";
+const url = "https://visits-christian-guardias-projects.vercel.app/lovingmypets/login";
 const message = document.getElementById("message");
 const sectionA = document.querySelector('.submit');
 const sectionB = document.querySelector('.articles');
