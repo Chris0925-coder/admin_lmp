ea622faf4ce99f07201714a7de07dfd75c6dfa8a
Chris0925-coder <christianguardia@outlook.es>
2026-04-17 06:06:35 -0500

    0

--- public/scripts/addArticle.js
+++ public/scripts/addArticle.js
@@ -1,137 +1,37 @@
 const addArticleBTN = document.getElementById("add-btn");
 const formB = document.getElementById("form-b");
-const formUpdate = document.getElementById('form-update');
+const formUpdate = document.getElementById("form-update");
 const token = getCookie("token");
 // const fileInput = document.getElementById("images-update");
 // const fileInput = document.querySelector("#filename");
 
 // const files = fileInput.files;
-const urlAddArticle = "https://visits-christian-guardias-projects.vercel.app/lovingmypets";
+const urlAddArticle =
+  "https://visits-christian-guardias-projects.vercel.app/lovingmypets";
 
 // const addForm = document.getElementById("form");
 const updateBTN = document.getElementById("content-btns");
-let item = updateBTN.querySelector('.item:nth-child(2)');
-let newDiv = document.createElement('button');
+let item = updateBTN.querySelector(".item:nth-child(2)");
+let newDiv = document.createElement("div");
 
+async function update(id) {
+  let formData = new FormData(formUpdate);
 
-function addArticle() {
-  formB.addEventListener("submit", async function (event) {
-    event.preventDefault();
-    addArticleBTN.disabled = true;
-    let formData = new FormData(formB);
+  console.log(formData);
 
-    // let jsonData =  { 
-    //   "title": formData.get("title"),  
-    //   "paragraph": formData.get("paragraph"), 
-    //   "link": formData.get("link"), 
-    //   "origin": formData.get("origin")
-    // };
-
-    // if (files.length > 0) {
-    //   formData.append("filename", fileInput.files[0]);
-    // }
-
-    // console.log(jsonData);
-
-    // const jsonData = { name: "John Doe", age: 30 };
-    // formData.append("metadata", new Blob([JSON.stringify(jsonData)], { type: "application/json" }));
-
-    // console.log(formData);
-
-    let result = await fetch(urlAddArticle, {
-      method: "POST",
-      headers: {
-        'Authorization': `Bearer ${token}`,
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Methods": "GET,HEAD,POST,OPTIONS",
-      },
-      body: formData,
-    })
-      .then((response) => {
-        console.log(response);
-        if (response.ok) {
-          alert("Added article successfully!");
-          window.location.reload();
-        } else {
-          alert("Failed to send the form submission.");
-        }
-      })
-      .catch((error) => console.error("Error:", error));
-      console.log(result);
-
-      if(result.message === 'Invalid token') {
-        removeCookie("token");
-        sectionB.setAttribute("class", "hidden");
-        sectionA.removeAttribute("class", "hidden");
-        return message.innerText = result.message + " Inicia sesion"; 
-      }
-  });
-}
-
-addArticle();
-
-async function getHome() {
-  let result = await fetch(urlAddArticle, {
-    method: "GET",
-      headers: {
-        'Authorization': `Bearer ${token}`,
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Methods": "GET,HEAD,POST,OPTIONS",
-      },
-  }).then((resp) => resp.json());
-
-  console.log(result);
-
-  result.forEach((btn) => {
-    // newDiv.innerHTML += `
-    //         <button value='${btn.id}'>Update: ${btn.title}</button>
-    //       `;
-    newDiv.innerText += btn.title;
-    newDiv.value += btn.id;
-    console.log(newDiv);
-
-    updateBTN.insertBefore(newDiv, item);
-    update(newDiv)
-
+  let result = await fetch(`${urlAddArticle}/${id}`, {
+    method: "PUT",
+    body: formData,
   })
-
+    .then((response) => {
+      if (response.ok) {
+        alert("Update article successfully!");
+      } else {
+        alert("Failed to update the form submission.");
+      }
+    })
+    .catch((error) => console.error("Error:", error));
 }
-
-getHome();
-
-function update(b) {
-  console.log(Object.values(b));
-  Object.values(b).forEach((id) => {
-    id.addEventListener("click", async function (event) {
-      event.preventDefault();
-      console.log(id)
-      // btn.disabled = true;
-      
-      let formData = new FormData(formUpdate);
-
-      let result = await fetch(`${urlAddArticle}/${id.value}`, {
-        method: "PUT",
-        // headers: {
-        //   "Content-Type": "application/json; charset=utf-8",
-        //   "Access-Control-Allow-Origin": "*",
-        //   "Access-Control-Methods": "GET,PUT,POST,OPTIONS",
-        // },
-        body: formData,
-      })
-        .then((response) => {
-          if (response.ok) {
-            alert("Update article successfully!");
-            window.location.reload();
-          } else {
-            alert("Failed to update the form submission.");
-          }
-        })
-        .catch((error) => console.error("Error:", error));
-    });
-  });
-}
-
-// const deleteBTN = document.getElementsByName("btn-delete");
 
 function deleteArticle() {
   deleteBTN.forEach((btn) => {
@@ -173,4 +73,84 @@ function deleteArticle() {
   });
 }
 
+function content(id, title) {
+  newDiv.setAttribute("class", "btns-id");
+  newDiv.innerHTML += `
+            <button value='${id}'>Update: ${title}</button>
+          `;
+  //   console.log(newDiv);
+  updateBTN.insertBefore(newDiv, item);
+  newDiv.addEventListener("click", (e) => {
+
+    if (e.target.tagName == "BUTTON") {
+      update(e.target.value);
+    }
+  });
+}
+
+function addArticle() {
+  formB.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    addArticleBTN.disabled = true;
+    let formData = new FormData(formB);
+
+    let result = await fetch(urlAddArticle, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Methods": "GET,HEAD,POST,OPTIONS",
+      },
+      body: formData,
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert("Added article successfully!");
+          window.location.reload();
+        } else {
+          alert("Failed to send the form submission.");
+        }
+      })
+      .catch((error) => console.error("Error:", error));
+
+    if (result.message === "Invalid token") {
+      removeCookie("token");
+      sectionB.setAttribute("class", "hidden");
+      sectionA.removeAttribute("class", "hidden");
+      return (message.innerText = result.message + " Inicia sesion");
+    }
+  });
+}
+
+async function getHome() {
+  let result = await fetch(urlAddArticle, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Methods": "GET,HEAD,POST,OPTIONS",
+    },
+  }).then((resp) => resp.json());
+
+  result.forEach((btn) => {
+
+    updateBTN.innerHTML += `
+            <button value='${btn.id}'>Update: ${btn.title}</button>
+          `;
+  });
+
+  updateBTN.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (e.target.tagName == "BUTTON") {
+      update(e.target.value);
+    }
+
+  });
+}
+
+
+
 // deleteArticle();
+addArticle();
+getHome();
