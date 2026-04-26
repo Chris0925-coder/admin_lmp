e53a659979c0ab8baaa152b3d9da404daf70d676
Chris0925-coder <christianguardia@outlook.es>
2026-04-26 12:15:01 -0500

    0

--- public/scripts/cookies.js
+++ public/scripts/cookies.js
@@ -3,11 +3,13 @@ function setCookie(cname, cvalue, exdays) {
         d.setTime(d.getTime() + (exdays*24*60*60*1000));
         let expires = "expires="+ d.toUTCString();
 
-        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+        document.cookie =  `${encodeURIComponent(cname)}=${encodeURIComponent(cvalue)};${expires};path=/`;
+
+        // cname + "=" + cvalue + ";" + expires + ";path=/";
     };
 
 function getCookie(cname) {
-    let name = cname + "=";
+    let name = encodeURIComponent(cname) + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
     for(let i = 0; i <ca.length; i++) {
@@ -23,12 +25,12 @@ function getCookie(cname) {
 };
 
 function removeCookie(cname){
-    getCookie(cname,"",-1);
+    setCookie(cname,"",-1);
 };
 
 
 function detectCookie(cname) {
-    let name = cname + "=";
+    let name =  encodeURIComponent(cname) + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
     for(let i = 0; i <ca.length; i++) {
