196b9d840795ba69e232dab6fbe74809423daf8c
Chris0925-coder <christianguardia@outlook.es>
2026-05-08 09:48:55 -0500

    0

--- public/scripts/addArticle.js
+++ public/scripts/addArticle.js
@@ -300,7 +300,7 @@ function addArticle() {
         formData.append("paragraph", articleParagraph);
 
         for (let i = 0; i < articleFiles.length; i++) {
-          formData.append("filename[]", articleFiles[i]);
+          formData.append("filename", articleFiles[i]);
         }
       }
 
