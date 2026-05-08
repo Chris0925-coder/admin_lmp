6821713078221b7a449089cfc1824af83579a1f9
Chris0925-coder <christianguardia@outlook.es>
2026-05-08 13:04:22 -0500

    0

--- public/scripts/addArticle.js
+++ public/scripts/addArticle.js
@@ -277,7 +277,7 @@ function addArticle() {
       formData = new FormData(formB);
       // formData.append("title", formData.get("title"));
       // formData.append("paragraph", formData.get("paragraph"));
-      formData.append("filename[]", formData.get("filename"));
+      formData.append("filename", formData.get("filename"));
       formData.append("link", formData.get("link"));
       // formData.get("filename")
     } else {
